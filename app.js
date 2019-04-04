9d77d688e05c40eec4dada36675c87bab2e36259
drakenm <drakenm@users.noreply.github.com>
2019-04-04 09:04:11 -0500

    starting point

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const e = require('events'),
+const events = require('events'),
   util = require('util');
 
 const fs = require('fs'),
@@ -6,10 +6,40 @@ const fs = require('fs'),
   processedDirectory = './done';
 
 
-class Watcher extends e.EventEmitter {
+class Watcher extends events.EventEmitter {
+
   constructor(watchDirectory, processedDirectory) {
     super();
     this.watchDirectory = watchDirectory;
     this.processedDirectory = processedDirectory;
   }
+
+  watch() {
+    const watcher = this;
+    fs.readdir(this.watchDirectory, function(err, files) {
+      if (err) throw err;
+      for (let index in files) {
+        watcher.emit('process', files[index]);
+      }
+    });
+  }
+
+  start() {
+    var watcher = this;
+    fs.watchFile(watchDirectory, function() {
+      watcher.watch();
+    })
+  }
 }
+
+let watcher = new Watcher(watchDirectory, processedDirectory);
+
+watcher.on('process', function process(file) {
+  const watchFile = this.watchDirectory + '/' + file;
+  const processedFile = this.processedDirectory + '/' + file.toLowerCase();
+  fs.rename(watchFile, processedFile, function (err) {
+    if (err) throw err;
+  });
+});
+
+watcher.start();
